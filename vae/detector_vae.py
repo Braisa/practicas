71d876847eb12225e22a79a086cce624fc65efa0
Braisa <oterolemabrais@gmail.com>
2025-06-26 11:35:32 +0200

    VAE loss weighing

--- vae/detector_vae.py
+++ vae/detector_vae.py
@@ -12,44 +12,55 @@ import pandas as pd # type: ignore
 import argparse
 from scipy.signal.windows import gaussian
 
-class VAE(nn.Module):
-    def __init__(self, latent_dim=12):
-        super(VAE, self).__init__()
-        self.latent_dim = latent_dim
-        self.encoder = nn.Sequential(
-            nn.Linear(132, 64),
+class Encoder(nn.Module):
+    def __init__(self, input_dim, hidden_dim, latent_dim):
+        super(Encoder, self).__init__()
+        self.encoding_layers = nn.Sequential(
+            nn.Linear(input_dim, hidden_dim),
             nn.ReLU(),
-            nn.Linear(64, 16),
+            nn.Linear(hidden_dim, hidden_dim),
             nn.ReLU()
         )
-        self.mean_layer = nn.Linear(16, self.latent_dim)
-        self.logvar_layer = nn.Linear(16, self.latent_dim)
-        self.decoder = nn.Sequential(
-            nn.Linear(self.latent_dim, 24),
+        self.mean_layer = nn.Linear(hidden_dim, latent_dim)
+        self.logvar_layer = nn.Linear(hidden_dim, latent_dim)
+    
+    def forward(self, x):
+        x = self.encoding_layers(x)
+        mean, logvar = self.mean_layer(x), self.logvar_layer(x)
+        return mean, logvar
+
+class Decoder(nn.Module):
+    def __init__(self, latent_dim, hidden_dim, output_dim):
+        super(Decoder, self).__init__()
+        self.decoding_layers = nn.Sequential(
+            nn.Linear(latent_dim, hidden_dim),
             nn.ReLU(),
-            nn.Linear(24, 64),
+            nn.Linear(hidden_dim, hidden_dim),
             nn.ReLU(),
-            nn.Linear(64, 132),
+            nn.Linear(hidden_dim, output_dim),
             nn.ReLU()
         )
     
-    def encode(self, x):
-        x = self.encoder(x)
-        mean, logvar = self.mean_layer(x), self.logvar_layer(x)
-        return mean, logvar
+    def forward(self, x):
+        x = self.decoding_layers(x)
+        return x
+
+class VAE(nn.Module):
+    def __init__(self, Encoder, Decoder, latent_dim=8):
+        super(VAE, self).__init__()
+        self.latent_dim = latent_dim
+        self.Encoder = Encoder
+        self.Decoder = Decoder
 
     def reparameterization(self, mean, var):
         epsilon = torch.randn_like(var)
         z = mean + var * epsilon
         return z
-    
-    def decode(self, x):
-        return self.decoder(x)
 
     def forward(self, x):
-        mean, logvar = self.encode(x)
-        z = self.reparameterization(mean, logvar)
-        t = self.decode(z)
+        mean, logvar = self.Encoder(x)
+        z = self.reparameterization(mean, torch.exp(.5*logvar))
+        t = self.Decoder(z)
         return t, mean, logvar
 
 class DetectorDataset(Dataset):
@@ -68,7 +79,7 @@ def loss_function(args, output, target):
     mse_loss = nn.functional.mse_loss(x, target)
     kld_loss = -.5 * torch.sum(1 + logvar - mu.pow(2) - logvar.exp())
 
-    return mse_loss + kld_loss
+    return 2 * (kld_loss + args.recon_loss_weight * (mse_loss - kld_loss))
 
 def train(args, model, loader, optimizer, epoch, no_print=False):
     initial_time = time()
@@ -101,7 +112,7 @@ def test(args, model, loader, no_print=False):
     test_loss = np.mean(losses)
     if not no_print:
         print(f"\033[K", end="\r")
-        print(f"Testing\t\tAverage loss: {test_loss:.6f}\t({time()-initial_time:.2f}s elapsed)")
+        print(f"Testing\t\t\t\tAverage loss: {test_loss:.6f}\t({time()-initial_time:.2f}s elapsed)")
     return test_loss
 
 def paint_losses(args, train_losses, test_losses, train_loader, test_loader):
@@ -121,24 +132,31 @@ def paint_losses(args, train_losses, test_losses, train_loader, test_loader):
     
     fig.savefig(f"vae/{args.folder_name}_figs/{args.save_name}_losses.pdf", dpi=300, bbox_inches="tight")
 
-def generate_graph(args, model, number=1):
+def generate_ax(args, model, ax, spots, minor=False):    
+    model.eval()
+    with torch.no_grad():
+        counts = model.Decoder(torch.randn(model.latent_dim))
+    
+    ax.plot(spots, counts, color = "tab:orange")
+
+    ax.set_xlim(left=1, right=33*4)
+    ax.set_ylim(bottom=0)
+
+    ax.xaxis.set_major_locator(plt.MultipleLocator(base=33))
+    if minor:
+        ax.xaxis.set_minor_locator(plt.MultipleLocator(1))
+
+def generate_graphs(args, model, number=1):
 
     fig, axs = plt.subplots(number, number, figsize=(1.5*number,1.5*number), sharex="all", layout="compressed")
 
     spots = np.arange(1, 1+33*4)
 
-    for ax in axs.ravel():
-        model.eval()
-        with torch.no_grad():
-            counts = model.decode(torch.randn(model.latent_dim))
-        
-        ax.plot(spots, counts, color = "tab:orange")
-
-        ax.set_xlim(left=1, right=33*4)
-        ax.set_ylim(bottom=0)
-
-        ax.xaxis.set_major_locator(plt.MultipleLocator(base=33))
-        #ax.xaxis.set_minor_locator(plt.MultipleLocator(1))
+    if number == 1:
+        generate_ax(args, model, axs, spots, minor=True)
+    else:
+        for ax in axs.ravel():
+            generate_ax(args, model, ax, spots, minor=False)
 
     fig.supxlabel("Detector position")
     fig.supylabel("Photon count")
@@ -177,34 +195,43 @@ def create_parser():
                         help="enable load mode, loading from save-name (default=False)")
     parser.add_argument("--print-number", type=int, default=1,
                         help="input printed outputs number, which will be squared (default=1)")
+    parser.add_argument("--latent-dim", type=int, default=8,
+                        help="input latent space dimensionality (default=8)")
+    parser.add_argument("--hidden-dim", type=int, default=32,
+                        help="input hidden layers dimensionality (default=32)")
+    parser.add_argument("--recon-loss-weight", type=float, default=.5,
+                        help="input reconstruction loss weight (default=0.5)")
     
     return parser
 
 def main():
     main_time = time()
+    print("Program start")
     
     parser = create_parser()
     args = parser.parse_args()
 
     torch.manual_seed(args.seed)
 
-    df = pd.DataFrame(pd.read_pickle("vae/simulated_events.pickle"))
-    photon_counts = torch.tensor(list(df[df.columns[-1]].values))
-    
-    train_counts, test_counts = train_test_split(photon_counts, test_size=.2, random_state=args.seed)
-
-    train_loader = DataLoader(
-        DetectorDataset(train_counts),
-        batch_size=args.train_batch_size, shuffle=True
-    )
-
-    test_loader = DataLoader(
-        DetectorDataset(test_counts),
-        batch_size=args.test_batch_size, shuffle=True
-    )
-
-    model = VAE()
+    enc, dec = Encoder(132, args.hidden_dim, args.latent_dim), Decoder(args.latent_dim, args.hidden_dim, 132)
+    model = VAE(Encoder=enc, Decoder=dec, latent_dim=args.latent_dim)
     if not args.load:
+        df = pd.DataFrame(pd.read_pickle("vae/simulated_events.pickle"))
+        photon_counts = torch.tensor(list(df[df.columns[-1]].values))
+        
+        train_counts, test_counts = train_test_split(photon_counts, test_size=.2, random_state=args.seed)
+
+        train_loader = DataLoader(
+            DetectorDataset(train_counts),
+            batch_size=args.train_batch_size, shuffle=True
+        )
+
+        test_loader = DataLoader(
+            DetectorDataset(test_counts),
+            batch_size=args.test_batch_size, shuffle=True
+        )
+        print("Data loaded")
+        
         match args.optim:
             case "Adam":
                 optimizer = torch.optim.Adam(model.parameters(), lr=args.learning_rate)
@@ -212,24 +239,28 @@ def main():
                 optimizer = torch.optim.SGD(model.parameters(), lr=args.learning_rate)
 
         train_losses, test_losses = [], []
-
+        print("Training start")
         for epoch in range(1, args.epochs+1):
             train_log = train(args, model, train_loader, optimizer, epoch)
             test_loss = test(args, model, test_loader)
 
             train_losses.append(train_log)
             test_losses.append(test_loss)
+        print("Training end")
 
         if args.save:
             torch.save(model.state_dict(), f"vae/{args.folder_name}_models/{args.save_name}_model.pt")
-
+            print("Model saved")
+        
         paint_losses(args, np.ravel(train_losses), test_losses, train_loader, test_loader)
+        print("Losses painted")
 
     else:
         model.load_state_dict(torch.load(f"vae/{args.folder_name}_models/{args.save_name}_model.pt"))
         model.eval()
 
-    generate_graph(args, model, number=args.print_number)
+    generate_graphs(args, model, number=args.print_number)
+    print("Graphs generated")
 
     main_total = time()-main_time
     main_days = int(main_total // 86400)
