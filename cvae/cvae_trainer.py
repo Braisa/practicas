bd99403029507542d62f8a1e73f671cd4609b406
Braisa <oterolemabrais@gmail.com>
2025-07-09 10:34:03 +0200

    cVAE better losses saving

--- cvae/cvae_trainer.py
+++ cvae/cvae_trainer.py
@@ -204,16 +204,20 @@ def save_losses(args, train_losses, test_losses, train_loader, test_loader):
     import matplotlib.ticker as ticker
     fig, ax = plt.subplots()
 
-    train_spots = np.arange(1, 1+args.epochs*(1+(len(train_loader.dataset)//args.train_batch_size))) * args.train_batch_size
-    test_spots = np.arange(1, 1+len(test_losses)) * len(train_loader.dataset)
+    #train_spots = np.arange(1, 1+args.epochs*(1+(len(train_loader.dataset)//args.train_batch_size))) * args.train_batch_size
+    #test_spots = np.arange(1, 1+len(test_losses)) * len(train_loader.dataset)
 
-    ax.plot(train_spots, train_losses, ls="solid", color="tab:blue", label="Train loss")
-    ax.plot(test_spots, test_losses, ls="solid", color="tab:orange", label="Test loss")
+    epoch_spots = np.arange(1, 1+args.epochs)
 
-    ax.xaxis.set_major_locator(plt.FixedLocator([i*len(train_loader.dataset) for i in range(0, 1+args.epochs)]))
-    ax.xaxis.set_major_formatter(ticker.FixedFormatter([f"{i}" for i in range(0, 1+args.epochs)]))
+    train_epoch_losses = [train_losses[spot] for spot in (np.arange(1, 1+args.epochs) * (len(train_loader.dataset)//args.train_batch_size))]
+
+    ax.plot(epoch_spots, train_epoch_losses, ls="solid", color="tab:blue", label="Train loss")
+    ax.plot(epoch_spots, test_losses, ls="solid", color="tab:orange", label="Test loss")
+
+    ax.xaxis.set_major_locator(plt.MultipleLocator(5))
+    ax.xaxis.set_minor_locator(plt.MultipleLocator(1))
     
-    ax.set_xlim(left=0)
+    ax.set_xlim(left=0, right=args.epochs+1)
     ax.set_xlabel("Epochs")
 
     ax.legend(loc="best")
@@ -222,10 +226,7 @@ def save_losses(args, train_losses, test_losses, train_loader, test_loader):
     Path(f"{full_path}").mkdir(parents=True, exist_ok=True)
     fig.savefig(f"{full_path}/{args.savename}_full_losses.pdf", dpi=300, bbox_inches="tight")
 
-    ax.set_xlim(left=len(train_loader.dataset))
-    top_lim = max(test_losses[1], train_losses[1+(len(train_loader.dataset)//args.train_batch_size)])
-    print(top_lim, test_losses[1])
-    ax.set_ylim(bottom=0, top=top_lim)
+    ax.set_xlim(left=1, right=args.epochs+1)
     fig.savefig(f"{full_path}/{args.savename}_losses.pdf", dpi=300, bbox_inches="tight")
 
 def train_for_epochs(args, model, optimizer, train_loader, test_loader, epochs, no_print=False):
